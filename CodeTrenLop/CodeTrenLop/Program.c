a4ca1154e6f62d0ce7ac6f85b9b60e9147765c62
PH67587 <11chutiendat@gmail.com>
2025-11-29 09:22:38 +0700

    Sắp xếp mảng

--- CodeTrenLop/CodeTrenLop/Program.c
+++ CodeTrenLop/CodeTrenLop/Program.c
@@ -3,22 +3,92 @@
 
 #include <stdio.h>
 
-int main()
-{
-    printf("Hi there!\n");
-}
 
-// Debug/Run chuong trinh: bam "F5" hoac "Debug > Start Debugging" tren menu
 
-// Danh cho nguoi moi bat dau:
-//   1. Dung "Solution Explorer window" de them/quan ly cac files
-//   2. Dung "Output window" de kiem tra "build output" va "cac thong bao khac"
-//   3. Dung "Error List window" de xem loi
-//   4. Chon "Project > Add New Item" de tao moi file code,
-//      hoac "Project > Add Existing Item" de them file da ton tai vao project nay.
-//   5. Sau nay, de mo lai project nay,
-//      chon "File > Open > Project" va chon file *.sln
-//      Co the xem thu muc project, noi luu file *.sln bang cach
-//      bam phim chuot phai vao "Solution" > chon "Open Folder in File Explorer"
+	void kiemTraSoNguyen()
+	{
+		printf("Kiem Tra So Nguyen");
+		printf("\n");
+	}
 
-// GV: AnhTT184
+	void sapXepPhanTuMang()
+	{
+		int integerArray[100];
+		int tmp;
+		int length;
+		printf("Nhap kich thuoc mang: ");
+		scanf_s("%d", &length);
+		printf("Nhap du lieu trong mang %d phan tu\n", length);
+		for (int i = 0; i < length; i++)
+		{
+			printf("mang[%d] = ", i);
+			scanf_s("%d", &integerArray[i]);
+		}
+		printf("Du lieu trong mang %d phan tu\n", length);
+		for (int i = 0; i < length; i++)
+		{
+			if (integerArray[i] > integerArray[i + 1])
+			{
+
+				tmp = integerArray[i];
+				integerArray[i] = integerArray[i + 1];
+				integerArray[i + 1] = tmp;
+			}
+		}
+		printf("Du lieu trong mang %d phan tu\n", length);
+		for (int i = 0; i < length - 1; i++)
+		{
+			printf("Mang[%d] = %d\n ", i, integerArray[i]);
+		}
+	}
+	void lapChucNang(int chonChucNang)
+	{
+		int tiepTuc = 1;
+		while (tiepTuc == 1)
+		{
+			switch (chonChucNang)
+			{
+			case 1:
+				kiemTraSoNguyen();
+				break;
+			case 2:
+				sapXepPhanTuMang();
+				break;
+			case 3:
+				// ham goi chuc nang 3
+				break;
+			default:
+				printf("Chon sai. Chuc nang hop le [0-3]");
+				break;
+			}
+
+			printf("Tiep tuc thuc hien chuc nang nay? [1=Co | 0=Khong]: ");
+			scanf_s("%d", &tiepTuc);
+			system("cls");
+		}
+
+	}
+
+	int main()
+	{
+		int chonChucNang;
+		do
+		{
+			printf("Menu");
+			printf("\n");
+			printf("1. Kiem Tra So Nguyen");
+			printf("\n");
+			printf("2. sapXepPhanTuMang");
+			printf("\n");
+			printf("3. TEN chuc nang 3");
+			printf("\n");
+			printf("0. Thoat");
+			printf("\n");
+			printf("Hay chon chuc nang [0-3]: ");
+			scanf_s("%d", &chonChucNang);
+			lapChucNang(chonChucNang);
+		} while (chonChucNang != 0);
+	}
+
+
+	
