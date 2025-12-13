fd758123ae9f5c9b7fa171e5c75851ba3473499d
PH67587 <11chutiendat@gmail.com>
2025-12-13 09:09:16 +0700

    bài test

--- ctd_test2/ctd_test2/Program.c
+++ ctd_test2/ctd_test2/Program.c
@@ -2,64 +2,60 @@
 // Chuong trinh phan mem Bat dau thuc thi & Ket thuc o day.
 
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
 
 void thongTinThuCung()
 {
 	char tenthucung[50];
-	char mathucung[30];
+	char mathucung[50];
 	int namsinh;
 	int year;
-	int currentYear = 2025;
-	while (getchar() != '\n');
+	int currentyear = 2025;
+
+	printf("Nhap ten thu cung: ");
+	scanf_s("%d", &tenthucung);
 	fgets(tenthucung, sizeof(tenthucung), stdin);
-	while (getchar() != '\n');
-	//tinh nam sinh
+
+	printf("Nhap ma thu cung: ");
+	scanf_s("%d", &mathucung);
+	//tinh
 	printf("Nhap nam sinh thu cung: ");
 	scanf_s("%d", &year);
-	//in ket qua
-	printf("%sTuoi: %d\n", tenthucung, currentYear - year);
+	printf("%stuoi: %d\n", tenthucung, currentyear - year);
+
 
 }
 
 void tinhTong()
 {
-	//nhap n
+	//tinh
 	int n, tong;
-	printf("Nhap so nguyen n(n > 3): ");
+	printf("Nhap so nguyen n (n > 3): ");
 	scanf_s("%d", &n);
 	//tinh tong
 	tong = n * (n + 1) / 2;
-	printf("tong tu 1 den :%d = %d\n", n, tong);
+	printf("tong tu 1 den: %d = %d\n", n, tong);
+	//kiem tra so chia het cho 5
 	if (n % 5 == 0)
 	{
-		printf("%d chia het cho 5\n", n);
+		printf("%d Chia het cho 5\n", n);
 	}
 	else
 	{
-		printf("%d khong chia het cho 5\n", n);
+		printf("%d Khong chia het cho 5\n", n);
 	}
 }
-
 
 void thongTinCuaHang()
 {
-	int n;
+	int soluong, cannang;
 	printf("Nhap so luong thu cung: ");
-	scanf_s("%d", &n);
-
-	float w, sum = 0, min;
-	for (int i = 0; i < n; i++) 
-	{
-		printf("Can nang %d: ", i + 1);
-		scanf_s("%f", &w);
-		sum += w;
-		if (i == 0 || w < min) min = w;
-	}
-
-	printf("TB: %.2f kg\nThap nhat: %.2f kg\n", sum / n, min);
+	scanf_s("%d", &soluong);
+	printf("Nhap can nang thu cung: ");
+	scanf_s("%d", &cannang);
 
 }
-
 
 void lapChucNang(int chonChucNang)
 {
@@ -109,5 +105,3 @@ int main()
 		lapChucNang(chonChucNang);
 	} while (chonChucNang != 0);
 }
-
-
